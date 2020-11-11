6ecf2483a23997b1a84263bc0f7aab8a90f0e35b
Thomas-RC <t-rossi@outlook.com>
2020-11-11 20:05:31 +0100

    Change auth in router

--- app/src/main/java/com/rosdesign/productsapp/ConfigsApp.java
+++ app/src/main/java/com/rosdesign/productsapp/ConfigsApp.java
@@ -2,9 +2,10 @@ package com.rosdesign.productsapp;
 
 public class ConfigsApp
 {
-    public static final String URI = "http://192.168.0.25";
+    public static final String URI = "https://bus.ros-design.com";
     public static final String API = URI+"/api";
     public static final String LOGIN = API+"/login";
     public static final String LOGOUT = API+"/logout";
     public static final String REGISTER = API+"/register";
 }
+
